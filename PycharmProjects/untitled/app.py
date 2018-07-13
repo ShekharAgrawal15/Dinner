31a55323e4af1f3d807d608d77b17c924e1cd0d5
saurabh <saurabh.jain@walmartlabs.com>
2018-07-13 14:25:13 -0700

    background

--- PycharmProjects/untitled/app.py
+++ PycharmProjects/untitled/app.py
@@ -9,22 +9,19 @@ app = Flask(__name__)
 @app.route('/')
 def hello_world():
     return render_template('home.html')
-    messagebox.askokcancel("Title","The application will be closed")
-    messagebox.askyesno("Title","Do you want to save?")
-    messagebox.askretrycancel("Title","Installation failed, try again?")
 
 @app.route('/', methods=['POST'])
 def login():
+    x = set()
+    a = []
     mtkid = request.form['mtkid']
     print(mtkid)
     processed_text = mtkid.upper()
-
     today = str(date.today())
     print(today)
     f = open(today, "a+")
     f.write(mtkid + "\n")
     f.close()
-
     return ('Thank you for choosing the dates!')
 
 if __name__=='__main__':
