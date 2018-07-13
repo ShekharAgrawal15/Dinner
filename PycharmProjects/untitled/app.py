63d7865de42dbc369dff3aa6b0d06d9640fbbb6d
Shekhar <Shekharagrawal92@gmail.com>
2018-07-12 21:29:22 -0700

    Added latest files

--- PycharmProjects/untitled/app.py
+++ PycharmProjects/untitled/app.py
@@ -1,17 +1,31 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+from datetime import date
+import tkinter
+from tkinter import messagebox
 
 app = Flask(__name__)
 
+
 @app.route('/')
-def hello_World():
-    print ('Hey')
+def hello_world():
     return render_template('home.html')
+    messagebox.askokcancel("Title","The application will be closed")
+    messagebox.askyesno("Title","Do you want to save?")
+    messagebox.askretrycancel("Title","Installation failed, try again?")
 
-@app.route('/', methods=['GET', 'POST'])
+@app.route('/', methods=['POST'])
 def login():
-    MTK_ID = request.form.get ('MTK_ID')
-    print (MTK_ID)
-    print ('HEYYYYYYYY')
+    mtkid = request.form['mtkid']
+    print(mtkid)
+    processed_text = mtkid.upper()
 
-if __name__ == '_main_':
+    today = str(date.today())
+    print(today)
+    f = open(today, "a+")
+    f.write(mtkid + "\n")
+    f.close()
+
+    return ('Thank you for choosing the dates!')
+
+if __name__=='__main__':
     app.run()
